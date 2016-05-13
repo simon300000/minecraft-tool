fa5a42cd5bfefe81f70be43303ec7a145b8c5c37
simon3000 <simon3000@163.com>
2016-05-13 14:46:34 +0800

    add something

--- main.js
+++ main.js
@@ -1,52 +1,36 @@
 'use strict'
 
 const electron = require('electron')
-// Module to control application life.
 const app = electron.app
-// Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
-
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+//const menubar = require('menubar')
+//let mb = menubar()
+let windows = []
 
 let createWindow = (a, b, c, d, e, f) => {
-    // Create the browser window.
-    mainWindow = new BrowserWindow({
+    windows.push(new BrowserWindow({
         width: 800,
         height: 600
-    })
+    }))
 
-    // and load the index.html of the app.
-    mainWindow.loadURL('file://' + __dirname + '/index.html')
+    const wid = windows.length - 1
 
-    // Open the DevTools.
-    mainWindow.webContents.openDevTools()
+    windows[wid].loadURL('file://' + __dirname + '/html/index.html')
 
-    // Emitted when the window is closed.
-    mainWindow.on('closed', function() {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
-        mainWindow = null
+
+    windows[wid].on('closed', function() {
+        delete windows[wid]
     })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
 app.on('ready', () => app.emit('activate'))
 
-// Quit when all windows are closed.
 app.on('window-all-closed', () => {
-    // On OS X it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
     if (process.platform !== 'darwin') {
         app.quit()
     }
 })
 
 app.on('activate', () => {
-    // On OS X it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
     createWindow()
 })
