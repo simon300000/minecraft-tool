575fe3f0f38dd0edbea3cb2dfb67f466bb82b6fd
simon3000 <simon3000@163.com>
2016-05-15 17:11:08 +0800

    小菜单

--- html/index.js
+++ html/index.js
@@ -1,12 +1,52 @@
 'use strict'
 const ipcRenderer = require('electron').ipcRenderer
-/*
-* test script start
-*/
+    /*
+     * test script start
+     */
 ipcRenderer.on('asynchronous-reply', function(event, arg) {
-  console.log(arg)
+    console.log(arg)
 });
 ipcRenderer.send('asynchronous-message', 'ping')
-/*
-* test script end
-*/
+    /*
+     * test script end
+     */
+let content = new ReactiveVar('home')
+
+Template.main.helpers({
+    home: () => {
+        if (content.get() == 'home') {
+            return 'active'
+        }
+    },
+    config: () => {
+        if (content.get() == 'config') {
+            return 'active'
+        }
+    },
+    about: () => {
+        if (content.get() == 'about') {
+            return 'active'
+        }
+    }
+});
+
+Template.content.helpers({
+    content: () => content.get()
+});
+
+
+
+Template.main.events({
+    'mousedown .home': (e) => {
+        content.set('home')
+    },
+    'mousedown .config': (e) => {
+        content.set('config')
+    },
+    'mousedown .about': (e) => {
+        content.set('about')
+    }
+});
+
+
+/* jshint -W117 */
